f8b5779fdc034fad50fec41ab323931ad3b8ab2d
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2021-10-19 01:37:50 +0330

    chore: added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
+
+    <nav class="navbarActive">
+        <h1>MENUE</h1>
+        <ul>
+          <li>ABOUT</li>
+          <li>MY SKILLS</li>
+          <li>EXPERIENCE / EDUCATION</li>
+          <li>CONTACS</li>
+          <li>test</li> 
+        </ul>
+      </nav>
+
     <section class="introImage">
       <div class="introText">
         <p>Hi I'm</p>
         <h1>Mehrnaz</h1>
         <h3>text sample</h3>
       </div>
+
+
+      
       
     </section>
+
   </div>
 
 </template>
@@ -21,6 +37,43 @@ export default {
 </script>
 
 <style>
+
+body{
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  perspective-origin: 0% 50%;
+  perspective: 800px;
+}
+
+*{
+  box-sizing: border-box;
+}
+
+/* nav , section{
+  transition: transform 150ms ease-out;
+} */
+
+nav{
+  z-index: 100;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 16em;
+  background-color: cornsilk;
+  transform: translateX(-16em);
+}
+
+nav.navbarActive{
+  transform: translateX(0);
+}
+
+nav.navbarHover{
+  transform: translateX(-15em);
+}
+
+
 .introImage{
   width: 100%;
   height: 100vh;
