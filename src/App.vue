77f7e1bdc3d659ad8048dac19d967c45e5c9ec29
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2021-10-18 14:19:30 +0330

    chore: worked on header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="introImage">
+      <div class="introText">
+        <p>Hi I'm</p>
+        <h1>Mehrnaz</h1>
+        <h3>text sample</h3>
+      </div>
+      
+    </section>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.introImage{
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient( rgba(0,0,0,0.3), transparent) , url(./assets/images/background.png);
+  background-size: cover;
+  background-position: center;
+  /* padding: 0 8%; */
+  position: relative;
+
 }
+
+.introText{
+  color: #fff;
+  position: absolute;
+  bottom: 8%;
+  /* background-color: blue; */
+  padding-left: 300px;
+}
+
 </style>
